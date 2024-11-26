94b6066bd7724ca30718329e00a9731d29bbfaf3
AnVogge <154121496+AnVogge@users.noreply.github.com>
2024-11-26 18:26:40 +0100

    Updated error with readme-file

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="tiktokscraper",            # Name of your package
     version="0.1.0",                 # Version of your package
     description="A package to collect TikTok videos, comments and user information, perform keyword search and collect the followers, followings and videos of a user.",  # Short description
-    long_description=open("README.md").read(),  # Long description from README
-    long_description_content_type="text/markdown",  # Format of the long description
+    long_description=open("README.rst").read(),  # Long description from README
+    long_description_content_type="text/x-rst",  # Format of the long description
     author="Angelina Voggenreiter",              # Your name
     author_email="angelina.voggenreiter@tum.de",  # Your email
     url="https://github.com/jpfefferlab/tiktokscraper",  # Link to your GitHub repo
