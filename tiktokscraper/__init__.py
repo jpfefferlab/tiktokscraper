5071b42445979391e78c7e5c9962cae101cd5cd7
AnVogge <154121496+AnVogge@users.noreply.github.com>
2024-11-26 18:38:26 +0100

    Fixed spelling mistake

--- tiktokscraper/__init__.py
+++ tiktokscraper/__init__.py
@@ -1,1 +1,1 @@
-from .scrape import get_video_details, get_profile_details, get_comments_from_video, start_selenium, get_videos_by_keyword, get_videos_by_user, get_followers_of_user, get_followings_of_user 
+from .scraper import get_video_details, get_profile_details, get_comments_from_video, start_selenium, get_videos_by_keyword, get_videos_by_user, get_followers_of_user, get_followings_of_user 
